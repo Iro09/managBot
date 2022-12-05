8300d1a624a3d02e2df6959dd1da68ddb10beae3
Pikachu <116916755+Iro09@users.noreply.github.com>
2022-12-05 17:28:44 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 if path.exists("config.env"):
     load_dotenv("config.env")
 
-BOT_TOKEN = environ.get("BOT_TOKEN", "5694028061:AAHlS1BqmOFk6IGEJQp_pPYiZgZFHuSDAO4")
-API_ID = int(environ.get("API_ID", "23315924"))
-API_HASH = environ.get("API_HASH", "3b39bf8e305df7b2f310ce6f53392ac6")
-SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "5722601919").split()]
-LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", "-1001848895379"))
-GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", "-1001848895379"))
-MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", "-1001616008828"))
+BOT_TOKEN = environ.get("BOT_TOKEN", "5663640542:AAFZGOOICAB5xl7PPKWF7VFI1ZJJh3m773Y")
+API_ID = int(environ.get("API_ID", "11796331"))
+API_HASH = environ.get("API_HASH", "a089161b52f234bb90a6eb915551e8c0")
+SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "5518757491").split()]
+LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", "-1001772857132"))
+GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", "-1001772857132"))
+MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", "-1001772857132"))
 WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", "300"))
 MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://deadterabaap09:dead@cluster0.3a4z5gq.mongodb.net/?retryWrites=true&w=majority")
 ARQ_API_URL = environ.get("ARQ_API_URL", "https://thearq.tech")
